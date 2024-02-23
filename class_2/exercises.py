a05f3b61018addf5f0fcf1acec02098e97cf1f24
Robinson <robinson.lovatto@gmail.com>
2024-02-23 22:59:59 +0000

    add exercises

---
+++ class_2/exercises.py
@@ -0,0 +1,8 @@
+# type 2 whole numbers and prints the division quotient of the first number divided by the second
+
+number_01 = int(input("Type the first number: "))
+number_02 = int(input("Type the second number: "))
+
+result = number_01 // number_02
+
+print(result)
